e3953d73387d7e7706799a072f89981dbb958f67
Yiming Yan Optimization <yanyiming1986@gmail.com>
2014-03-13 23:40:22 +0000

    Update qps2mat.m. Test it tomorrow.

--- qps2mat.m
+++ qps2mat.m
@@ -1,4 +1,4 @@
-function qps2mat()
+function qps2mat(folderName)
 % QPS2MAT This function makes use of the coinRead function from OPTI to
 % read the QPS file and then save the corresponding problem as a mat file.
 %
@@ -11,32 +11,20 @@ function qps2mat()
 %   s.t. rl <= Ax <= ru
 %        lb <= x  <= ub.
 %
-% For most problems, rl is the same as ru, which means the problems are
-% in the following format
+% We save the data Q, c, A, rl, ru, lb and ub.
 %
-% 	min 0.5 x'Qx + c'x
-%   s.t. Ax = b 					(setting b = rl)
-%        lb <= x  <= ub.
-%
-% If so, this function will set b = rl (or ru) and save just Q, A, b, lb and ub.
-% These problems are consistent with the discrption from I. Maros, Cs. Meszaros.
-%
-% *** BUT we do observe that for some problems, rl ~= ru. ***
-% You will see some problems with rl = -inf and ru = +inf, which implies that 
-% Ax is free and there is no need to have A.
-%
-% Curently I have no idea whether it is a bug in coinRead or these problems are
-% just like this.
-%
-%     *For this case I will save all infomation, namely 
-%      Q, A, ru, rb, lb and ub* 
-% 
 % Yiming Yan
 % University of Edinburgh
 % 01 November 2013
 clc;
 
-files = dir( '*.QPS' );
+if ~ispc
+    error('QPS2MAT: Works only on windows system.');
+end
+
+if nargin < 1
+	files = dir( 'QPS_Files\*.QPS' );
+end
 % files = dir( 'DPKLO1.QPS' );
 numProb = length(files);
 fprintf( 'In total %d problem detected.\n', numProb )
@@ -49,23 +37,32 @@ for i = 1:numProb
     %% Assign data
     c  = p.f;
     A  = p.A;      Q = p.H;
-    ru = p.ru;    rl = p.rl;
+    rl = p.rl;    ru = p.ru;
     lb = p.lb;    ub = p.ub;
+ 	
+ 	clear p;
+ 	
+ 	%% Save data
+ 	save( ['MAT_Files\' name(1:end-3) 'mat'], 'Q', 'c', 'A', 'rl', 'ru', 'lb', 'ub');
  	
 	%% Check rl and ru
 	rl_is_ru = all(ru == rl);
 	
-	%% Save data   
  	if rl_is_ru
-		b = rl;
-    	save( ['MAT_Files\' name(1:end-3) 'mat'], 'Q', 'A', 'b', 'c', 'lb', 'ub');
+		fprint('rl = ru; Ax = b with b = rl; ')
 	else
 		fprintf('rl ~= ru; ');
 		rl_ru_inf = all(isinf(rl).*isinf(ru));
 		if rl_ru_inf
-			fprintf('rl=-inf, ru=_inf, Ax free; ');
+			fprintf('rl = -inf, ru = +inf, Ax free; ');
 		else
-
+			if all(inf(rl))
+				fprintf('rl = -inf, ru ~= +inf; ');
+			elseif all(inf(ru))
+				fprintf('rl ~= -inf, ru = +inf');
+			else
+				fprintf('rl ~= -inf, ru ~= +inf');
+			end
 		end
 	end
     
